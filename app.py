095a63f8586af6931a810946e203f9cf56c3fcb7
AmeenKhan12345 <ameenkhan2016yo@gmail.com>
2025-05-16 15:56:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
-import spacy
 import re
 import string
 from lime.lime_text import LimeTextExplainer
